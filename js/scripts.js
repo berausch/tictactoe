97951b414999eb23236a51f9b16640c2dac35fd7
Brittany Rausch and Josh Bertsche <brittany@>
2016-04-05 17:21:31 -0700

    Make a website to buy fake movie tickets

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,102 @@
 //business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
+function Ticket(movie, time, age) {
+    this.movie = movie;
+    this.time = time;
+    this.age = age;
 }
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+
+function Movie(title, rating, date, times) {
+    this.title = title;
+    this.rating = rating;
+    this.date = date;
+    this.times = times;
+}
+
+Ticket.prototype.price = function() {
+
+    var price = 10
+
+    if (this.age < 18) {
+        price -= 2
+    } else if (this.age >= 65) {
+        price -= 3
+    }
+    console.log(this.age);
+    console.log(price);
+    if (this.time === "0") {
+        price -= 3
+    } else if (this.time === "1") {
+        price -= 2
+    } else if (this.time === "2") {
+        price += 2
+    }
+    console.log(price);
+    if (this.movie.date === "A") {
+        price += 2
+    } else if (this.movie.date === "C") {
+        price -= 2
+    }
+    return price
 };
 
-//user interface logic
+Ticket.prototype.youth = function() {
+    if (this.movie.rating === "R" && this.age < 18) {
+        return false
+    } else {
+        return true
+    }
+}
+Ticket.prototype.batmanSucks = function() {
+  console.log(this.movie.title);
+    if (this.movie.title === "Batman v Superman") {
+        return false
+    } else {
+        return true
+    }
+}
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
+  var batman = new Movie("Batman v Superman", "PG-13", "A");
+  var wtf = new Movie("Whiskey Tango Foxtrot", "R", "A");
+  var allegiant = new Movie("The Divergent Series: Allegiant", "PG-13", "B");
+  var zootopia = new Movie("Zootopia", "PG-13", "B");
+  var deadpool = new Movie("Deadpool", "R", "C");
+  var kungFu = new Movie("Kung Foo Panda 3", "G", "C");
+  var cloverfield = new Movie("10 Cloverfield Lane", "R", "B");
+  var movies = [batman, wtf, allegiant, zootopia, deadpool, kungFu, cloverfield];
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    console.log(inputtedFirstName);
-    var inputtedLastName = $("input#new-last-name").val();
+  var newTicket;
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    //user interface logic
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+    $("form#movie-ticket").submit(function(event) {
+        event.preventDefault();
+        $(".young").hide();
+        $("#batman-sucks").hide();
+
+        var inputtedTitle = parseInt($("#movie-title").val());
+        var inputtedTime = $("#movie-time").val();
+        var inputtedAge = $("#age").val();
+        newTicket = new Ticket(movies[inputtedTitle], inputtedTime, inputtedAge);
+
+        if(newTicket.batmanSucks() === false) {
+          $("#batman-sucks").show();
+        }
+        else if (newTicket.youth() === true) {
+                var newPrice = newTicket.price();
+                $("#price").empty().append("$" + newPrice + " " + newTicket.movie.title);
+                $("#age").val("");
+            } else {
+            $(".young").show();
+        }
+        console.log(newPrice);
+
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-  });
-
+    $("form#batman-sucks").submit(function(event) {
+        event.preventDefault();
+        newTicket.movie = kungFu;
+        var newPrice = newTicket.price();
+        $("#price").empty().append("$" + newPrice + " " + newTicket.movie.title);
+        $("#batman-sucks").hide();
+    });
 });
