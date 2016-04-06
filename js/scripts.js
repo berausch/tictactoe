08d1fa806efe5b17952f5ea817ea0b3cdf2f92b2
Brittany Rausch <berausch247@gmail.com>
2016-04-06 15:43:24 -0700

    make a tic-tac-toe game

--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,168 @@
 //business logic
-function Ticket(movie, time, age) {
-    this.movie = movie;
-    this.time = time;
-    this.age = age;
+function Player(name, weapon, number) {
+    this.name = name;
+    this.weapon = weapon;
+    this.number = number;
+}
+function Board(row1, row2, row3) {
+    this.row1 = ["1a", "1b", "1c"];
+    this.row2 = ["2a", "2b", "2c"]];
+    this.row3 = ["3a", "3b", "3c"]];
 }
 
-function Movie(title, rating, date, times) {
-    this.title = title;
-    this.rating = rating;
-    this.date = date;
-    this.times = times;
+
+var currentPlayer;
+
+Board.prototype.playerTurn = function () {
+  var xCount = 0;
+  var oCount = 0;
+for (var i = 0; i < 3; i++){
+if(newBoard.row1[i] === "X"){
+  xCount += 1;
+} else if (newBoard.row1[i] === "O") {
+  oCount += 1;
 }
-
-Ticket.prototype.price = function() {
-
-    var price = 10
-
-    if (this.age < 18) {
-        price -= 2
-    } else if (this.age >= 65) {
-        price -= 3
-    }
-    console.log(this.age);
-    console.log(price);
-    if (this.time === "0") {
-        price -= 3
-    } else if (this.time === "1") {
-        price -= 2
-    } else if (this.time === "2") {
-        price += 2
-    }
-    console.log(price);
-    if (this.movie.date === "A") {
-        price += 2
-    } else if (this.movie.date === "C") {
-        price -= 2
-    }
-    return price
+if(newBoard.row2[i] === "X"){
+  xCount += 1;
+} else if (newBoard.row2[i] === "O") {
+  oCount += 1;
+}
+if(newBoard.row3[i] === "X"){
+  xCount += 1;
+} else if (newBoard.row3[i] === "O") {
+  oCount += 1;
+}
+}
+console.log(xCount);
+console.log(oCount);
+if (xCount === oCount) {
+  return true;
+} else if (xCount > oCount) {
+  return false;
+}
 };
-
-Ticket.prototype.youth = function() {
-    if (this.movie.rating === "R" && this.age < 18) {
-        return false
-    } else {
-        return true
-    }
-}
-Ticket.prototype.batmanSucks = function() {
-  console.log(this.movie.title);
-    if (this.movie.title === "Batman v Superman") {
-        return false
-    } else {
-        return true
-    }
-}
 $(document).ready(function() {
-  var batman = new Movie("Batman v Superman", "PG-13", "A");
-  var wtf = new Movie("Whiskey Tango Foxtrot", "R", "A");
-  var allegiant = new Movie("The Divergent Series: Allegiant", "PG-13", "B");
-  var zootopia = new Movie("Zootopia", "PG-13", "B");
-  var deadpool = new Movie("Deadpool", "R", "C");
-  var kungFu = new Movie("Kung Foo Panda 3", "G", "C");
-  var cloverfield = new Movie("10 Cloverfield Lane", "R", "B");
-  var movies = [batman, wtf, allegiant, zootopia, deadpool, kungFu, cloverfield];
-
-  var newTicket;
-
-
-    //user interface logic
-
-    $("form#movie-ticket").submit(function(event) {
+      $("#player").submit(function(event) {
         event.preventDefault();
-        $(".young").hide();
-        $("#batman-sucks").hide();
 
-        var inputtedTitle = parseInt($("#movie-title").val());
-        var inputtedTime = $("#movie-time").val();
-        var inputtedAge = $("#age").val();
-        newTicket = new Ticket(movies[inputtedTitle], inputtedTime, inputtedAge);
-
-        if(newTicket.batmanSucks() === false) {
-          $("#batman-sucks").show();
+        var inputtedName1 = $("#player-1-name").val();
+        var inputtedWeapon1 = $("#player-1-weapon").val();
+        var inputtedName2 = $("#player-2-name").val();
+        var inputtedWeapon2 = "";
+        if (inputtedWeapon1 === "X") {
+          inputtedWeapon2 = "O"
+        } else {
+          inputtedWeapon2 = "X"
         }
-        else if (newTicket.youth() === true) {
-                var newPrice = newTicket.price();
-                $("#price").empty().append("$" + newPrice + " " + newTicket.movie.title);
-                $("#age").val("");
-            } else {
-            $(".young").show();
-        }
-        console.log(newPrice);
+        var newPlayer1 = new Player(inputtedName1, inputtedWeapon1, 1);
+        var newPlayer2 = new Player(inputtedName2, inputtedWeapon2, 2);
 
+        newBoard = new Board("row1", "row2", "row3");
+
+        $(".1a").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row1[0]= currentPlayer.weapon;
+          $("#1a").text(currentPlayer.weapon);
+          console.log(newBoard);
+        });
+        $(".1b").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row1[1]= currentPlayer.weapon;
+          $("#1b").text(currentPlayer.weapon);
+          console.log(newBoard);
+        });
+        $(".1c").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row1[2]= currentPlayer.weapon;
+          $("#1c").text(currentPlayer.weapon);
+          console.log(newBoard);
+        });
+        $(".2a").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row2[0]= currentPlayer.weapon;
+          $("#2a").text(currentPlayer.weapon);
+        });
+        $(".2b").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row2[1]= currentPlayer.weapon;
+          $("#2b").text(currentPlayer.weapon);
+        });
+        $(".2c").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row2[2]= currentPlayer.weapon;
+          $("#2c").text(currentPlayer.weapon);
+        });
+        $(".3a").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row3[0]= currentPlayer.weapon;
+          $("#3a").text(currentPlayer.weapon);
+        });
+        $(".3b").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row3[1]= currentPlayer.weapon;
+          $("#3b").text(currentPlayer.weapon);
+        });
+        $(".3c").click(function(){
+          currentPlayer = newBoard.playerTurn();
+          if(currentPlayer === true){
+            currentPlayer = newPlayer1
+          } else if(currentPlayer === false){
+            currentPlayer = newPlayer2
+          }
+          console.log(currentPlayer);
+          newBoard.row3[2]= currentPlayer.weapon;
+          $("#3c").text(currentPlayer.weapon);
+        });
+        console.log(newBoard);
     });
-    $("form#batman-sucks").submit(function(event) {
-        event.preventDefault();
-        newTicket.movie = kungFu;
-        var newPrice = newTicket.price();
-        $("#price").empty().append("$" + newPrice + " " + newTicket.movie.title);
-        $("#batman-sucks").hide();
-    });
+
+
 });
